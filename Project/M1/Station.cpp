25d40f20651f1c4ffb91963d8d6b688759193dbd
Hkcs <hodakarimi980@gmail.com>
2025-05-02 20:06:17 -0400

    Delete Project/M1 directory

--- Project/M1/Station.cpp
+++
@@ -1,8 +0,0 @@
-#include "Station.h"
-
-namespace seneca {
-
-	size_t Station::m_widthField = 0; // initializes the static member
-	int Station::id_generator = 0; // initializes the static member
-
-}
